052a565376e931c3d644554fd0eef1f98963c73d
Basil Suter <git@nadar.io>
2018-07-09 13:26:39 +0200

    add redirect details.

--- src/modules/cms/models/NavItemRedirect.php
+++ src/modules/cms/models/NavItemRedirect.php
@@ -6,5 +6,22 @@ use luya\headless\base\BaseModel;
 
 class NavItemRedirect extends BaseModel
 {
+    const TYPE_INTERNAL_PAGE = 1;
     
+    const TYPE_EXTERNAL_URL = 2;
+    
+    const TYPE_LINK_TO_FILE = 3;
+    
+    const TYPE_LINK_TO_EMAIL = 4;
+    
+    const TYPE_LINK_TO_TELEPHONE = 5;
+    
+    public $id;
+    public $type;
+    public $value;
+
+    public function getUrl()
+    {
+        return $this->value;
+    }
 }
