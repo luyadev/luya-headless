fc89ebd668990c9d321772f631c24aa88aab4215
Basil Suter <git@nadar.io>
2018-09-12 15:46:04 +0200

    add storage models

---
+++ src/modules/admin/models/StorageFile.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace luya\headless\modules\admin\models;
+
+use luya\headless\base\BaseModel;
+
+/**
+ * Admin Storage File Model.
+ * 
+ * Expands:
+ * + source
+ * 
+ * @since 1.0.0
+ */
+class StorageFile extends BaseModel
+{
+    public $id;
+    public $is_hidden;
+    public $folder_id;
+    public $name_original;
+    public $new_new;
+    public $new_new_compound;
+    public $mime_type;
+    public $extension;
+    public $hash_file;
+    public $hash_name;
+    public $upload_timestamp;
+    public $file_size;
+    public $upload_user_id;
+    public $is_deleted;
+    public $passtrough_file;
+    public $passtrough_file_password;
+    public $passtrough_file_stats;
+    public $caption;
+    public $inline_disposition;
+    public $source;
+}
---
+++ src/modules/admin/models/StorageImage.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace luya\headless\modules\admin\models;
+
+use luya\headless\base\BaseModel;
+
+/**
+ * Admin Storage Image Model.
+ * 
+ * Expands:
+ * + source
+ * 
+ * @since 1.0.0
+ */
+class StorageImage extends BaseModel
+{
+    public $id;
+    public $file_id;
+    public $filter_id;
+    public $resolution_width;
+    public $resolution_height;
+    public $source;
+}
