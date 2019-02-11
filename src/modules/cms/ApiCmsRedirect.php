d4770ff8910210adeeca3cd5a6a12b896d14013a
Basil Suter <git@nadar.io>
2019-02-11 15:51:05 +0100

    add redirect

---
+++ src/modules/cms/ApiCmsRedirect.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace luya\headless\modules\cms;
+
+use luya\headless\ActiveEndpoint;
+
+/**
+ * Find redirect pages defined in redirects menu.
+ * 
+ * ```php
+ * $find = ApiCmsRediect('find/path', $client);
+ * 
+ * if ($find) {
+ *     // redirect to:
+ *     header("Location: " .$find->redirect_path);
+ * }
+ * ```
+ * 
+ * @since 1.1.0
+ * @author Basil Suter <basil@nadar.io>
+ */
+class ApiCmsRedirect extends ActiveEndpoint
+{
+    public $id;
+    public $timestamp_create;
+    public $catch_path;
+    public $redirect_path;
+    public $redirect_status_code;
+    
+    public function getEndpointName()
+    {
+        return '{{%api-cms-redirect}}';
+    }
+
+    public static function catch($path, Client $client)
+    {
+        return self::find()->setEndpoint('{endpointName}/catch')->setArgs(['path' => $path])->one($client);
+    }
+}
